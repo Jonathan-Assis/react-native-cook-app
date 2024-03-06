75a24409ddd2bc4c8b5a0c7ed145559749dcb378
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2024-03-05 21:13:14 -0300

    Removed initial structure

---
+++ src/app/index/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export default function Index() {
+    return (
+        <View>
+            <Text>React native na prática</Text>
+        </View>
+    )
+}
